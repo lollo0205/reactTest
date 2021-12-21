7e97ed899ee53650e9484ca8e9dacd66612d7a6b
beltramol <loris.beltramo@dncsrl.com>
2021-12-21 17:07:12 +0100

    clear tect card title

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -7,7 +7,7 @@ class Card extends Component {
         <div className="card" style={{ width: '18rem', textAlign: 'center' }}>
           <img src={this.props.card.image} className="card-img-top mx-auto d-block" style={{ height: '10rem', width: '17rem' }} alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{this.props.card.name}ryzen 5 5600G</h5>
+            <h5 className="card-title">{this.props.card.name}</h5>
             <p className="card-text">{this.props.card.price}€</p>
             <p className="card-text bold"><b>Stock:</b> {this.props.card.qtaStock}</p>
             <button className="btn btn-primary me-2" onClick={() => this.props.onAddProducts(this.props.card)}>add cart</button>
